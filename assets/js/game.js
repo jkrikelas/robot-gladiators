9c4d9416fbb00ec3bcac7a1c5160ef0e7f1cb3ea
jkrikelas <jamesthebluesman87@gmail.com>
2020-12-16 21:45:31 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,7 +25,7 @@ var startGame = function () {
             var pickedEnemyName = enemyNames[i];
 
             // reset enemyHealth before starting new fight
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
 
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
@@ -60,7 +60,12 @@ var endGame = function () {
         window.alert('Thank you for playing Battlebots! Come back soon!');
     }
 };
-
+//function to generate a random numberic value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+    
+    return value;
+};
 // fight function (now with parameter for enemy's name)
 var fight = function (enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
@@ -76,14 +81,18 @@ var fight = function (enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + ' has decided to skip this fight. Goodbye!');
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 shop();
                 break;
             }
         }
 
         // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        //enemyHealth = Math.max(0, enemyHealth - playerAttack);
+        //generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
             playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
         );
@@ -110,7 +119,10 @@ var fight = function (enemyName) {
         }
 
         // remove players's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        //playerHealth = Math.max(0, playerHealth - enemyAttack);
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
             enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
         );
@@ -160,3 +172,8 @@ var shop = function () {
 
 // start first game when page loads
 startGame();
+console.log(Math.PI);
+
+console.log(Math.round(4.4));
+
+console.log(Math.sqrt(25));
